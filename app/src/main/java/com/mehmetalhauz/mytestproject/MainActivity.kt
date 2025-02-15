ece1517a2503d1986714370d4b2675c4a14af280
Mehmet Talha UZ <mtu8697@gmail.com>
2025-02-15 16:09:18 +0300

    for loop added

---
+++ app/src/main/java/com/mehmetalhauz/mytestproject/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.mehmetalhauz.mytestproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val myList = arrayListOf(10,20,30,40)
+        for(num in myList){
+            println(num)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
